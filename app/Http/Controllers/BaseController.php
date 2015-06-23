738cc107a0e7b68790c744260a1552e5d5f275a6
Sean Hammon <sean.hammon@stgconsulting.com>
2015-06-23 12:24:03 -0600

    some basic parameter processing

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,6 +1,26 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Phogra\Exception\InvalidParameterException;
+
 class BaseController extends Controller {
+
+	protected $requestParams;
+	private $allowedParams = ['include','page','sort','filter','fields'];
+
+	public function __construct(Request $request) {
+		$this->requestParams = (object)[
+			'include' => [],
+			'page' => null,
+			'sort' => null,
+			'filter' => [],
+			'fields' => []
+		];
+		$this->processParams($request);
+	}
+
 
 	/**
 	 * Setup the layout used by the controller.
@@ -15,4 +35,58 @@ class BaseController extends Controller {
 		}
 	}
 
+	private function processParams(Request $request) {
+		$get = $request->all();
+		$incoming = array_keys($get);
+		$disallowed = array_diff($incoming, $this->allowedParams);
+		if (count($disallowed)) {
+			throw new InvalidParameterException("Unrecognized parameters:" . implode(",", $disallowed));
+		}
+		foreach ($get as $key => $value) {
+			switch ($key) {
+				case 'include':
+					$this->processInclude($value);
+				break;
+
+				case 'page':
+					$this->processPaging($value);
+				break;
+
+				case 'sort':
+					$this->processSort($value);
+				break;
+
+				case 'filter':
+					$this->processFilters($value);
+				break;
+
+				case 'fields':
+					$this->processFields($value);
+				break;
+			}
+		}
+	}
+
+	private function processInclude($value) {
+		$pairs = explode(',', $value);
+		foreach ($pairs as $p) {
+			$this->requestParams->include[] = explode('.', $p);
+		}
+	}
+
+	private function processPaging($value) {
+		$this->requestParams->page = $value;
+	}
+
+	private function processSort($value) {
+		$this->requestParams->sort = [$value];
+	}
+
+	private function processFilters($value) {
+
+	}
+
+	private function processFields($value) {
+		$this->requestParams->fields[] = $value;
+	}
 }
---
+++ app/Phogra/Exception/InvalidParameterException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Phogra\Exception;
+
+class InvalidParameterException extends PhograException
+{
+    public function __construct($message, $code = 400, \Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
