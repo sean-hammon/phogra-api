7fcfa2818f6f60e9601a3bfc9e26e886aefc9f18
Sean Hammon <sean.hammon@stgconsulting.com>
2015-07-13 09:13:06 -0600

    add the tree node column to the gallery table

--- database/migrations/2014_11_02_153102_CreateGalleriesTable.php
+++ database/migrations/2014_11_02_153102_CreateGalleriesTable.php
@@ -17,6 +17,7 @@ class CreateGalleriesTable extends Migration {
 			$table->integer('parent_id')->nullable();
 			$table->string('title', 64);
 			$table->string('slug', 64)->unique();
+			$table->string('node', 128)->unique();
 			$table->text('description')->nullable();
 			$table->tinyInteger('protected')->default(0);
 			$table->timestamps();
