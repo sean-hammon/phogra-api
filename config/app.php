833c9f1b535ad9fe3073218d7b24a7253d274862
Sean Hammon <sean.hammon@stgconsulting.com>
2015-09-04 10:52:34 -0600

    Add hashids library to obfuscate row ids

--- config/app.php
+++ config/app.php
@@ -144,7 +144,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
-
+        Vinkla\Hashids\HashidsServiceProvider::class,
     ],
 
     /*
@@ -192,9 +192,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
+        'JWTAuth'    => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Hashids'    => Vinkla\Hashids\Facades\Hashids::class,
     ],
 
 ];
