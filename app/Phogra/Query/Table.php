6b8eb2285ffc9a49b7f6d1e7326fe1d6d378e746
Sean Hammon <sean.hammon@stgconsulting.com>
2015-09-09 09:41:42 -0600

    I'm not liking having table names scattered all over the code. I'll clean up as I go.

---
+++ app/Phogra/Query/Table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Phogra\Query;
+
+class Table
+{
+    const photos = "photos";
+    const galleries = "galleries";
+    const gallery_photos = "gallery_photos";
+    const files = "files";
+}
