86bcaf4a38a9f4bcc85e79c6805179d2dcd821ba
Sean Hammon <git@sean-hammon.com>
2015-10-16 09:37:26 -0600

    Move to be with the rest of the exceptions, update to extend PhograException

---
+++ app/Phogra/Exception/DuplicateFileException.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Phogra\Exception;
+
+class DuplicateFileException extends PhograException
+{
+    public function __construct($message, $code = 400, \Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
--- app/Phogra/File/DuplicateFileException.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-namespace App\Phogra\File;
-
-class DuplicateFileException extends \Exception
-{
-
-}
