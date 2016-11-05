e90941b510578a7c28cb09c79137daacfdb2d586
Sean Hammon <git@sean-hammon.com>
2016-11-04 21:48:50 -0600

    Add tag related tables to Tables class.

--- app/Phogra/Query/Table.php
+++ app/Phogra/Query/Table.php
@@ -5,7 +5,9 @@ namespace App\Phogra\Query;
 class Table
 {
     const photos = "photos";
+	const photo_tags = "photo_tags";
     const galleries = "galleries";
     const gallery_photos = "gallery_photos";
     const files = "files";
+	const tags = "tags";
 }
