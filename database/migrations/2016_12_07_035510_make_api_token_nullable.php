e68fa46fb8cf3a4948c39aad4587bf914dfaafa0
Sean Hammon <git@sean-hammon.com>
2016-12-06 20:59:27 -0700

    I must have done this manually at one point and it fell over when setting up a new database.

---
+++ database/migrations/2016_12_07_035510_make_api_token_nullable.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeApiTokenNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+	    DB::statement('ALTER TABLE `users` CHANGE COLUMN `api_token` `api_token` VARCHAR(60) NULL DEFAULT NULL');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+	    DB::statement('ALTER TABLE `users` CHANGE COLUMN `api_token` `api_token` VARCHAR(60) NOT NULL');
+    }
+}
