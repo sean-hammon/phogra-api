93d1d85f72901791030e53c1b7b76b29bef7448b
Sean Hammon <git@sean-hammon.com>
2017-02-12 21:29:49 -0700

    Migration to add featured column to galleries table.

---
+++ database/migrations/2017_02_13_041136_add-featured-column-to-galleries.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFeaturedColumnToGalleries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->boolean('featured')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropColumn('featured');
+        });
+    }
+}
